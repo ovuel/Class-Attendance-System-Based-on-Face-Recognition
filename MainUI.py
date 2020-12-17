4bb81bcb7d25c39a3bace2bc1282484795c4840b
ovuel <paaSun@outlook.com>
2020-12-17 19:49:50 +0800

    '提交'

---
+++ MainUI.py
@@ -0,0 +1,34 @@
+# -*- coding: cp936 -*-
+#test
+#create a main page
+import kaoqin2
+import manager03
+import Window04
+from Tkinter import*
+ #×¢ÒâÒª´óÐ´
+def kaoqin(event):
+    kaoqin2.check_Page()
+def database(event):
+    manager03.db_Page()
+def enroll(event):
+    Window04.enroll_Page()
+    
+MainUI=Tk()
+MainUI.geometry("200x300")
+MainUI.title("Main page")
+MainUI['background']='LightSlateGray'
+
+B1=Button(MainUI,text='Check-in')
+B1.pack(pady=20)
+B1.bind("<Button-1>",kaoqin)
+
+B2=Button(MainUI,text='Database')
+B2.pack()
+B2.bind("<Button-1>",database)
+
+B3=Button(MainUI,text='Enroll')
+B3.pack(pady=20)
+B3.bind("<Button-1>",enroll)
+
+MainUI.mainloop()
+
